bfc17c864328156c52c3b21a981f724cd07e56c1
melaniyakireeva <melaniya.kireeva@bk.ru>
2025-05-26 13:00:39 +0300

    fix 8

--- WebLibraryApp/backend/LibraryApi/obj/Debug/net8.0/LibraryApp.AssemblyInfo.cs
+++ WebLibraryApp/backend/LibraryApi/obj/Debug/net8.0/LibraryApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26b530380d42c79388e5cc87830842cf85f07cde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
