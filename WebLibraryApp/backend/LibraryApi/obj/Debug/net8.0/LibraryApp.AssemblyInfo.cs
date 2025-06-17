14d283ef9eb33542ad139189c1ec58bcdbb916a1
melaniyakireeva <melaniya.kireeva@bk.ru>
2025-06-17 22:40:58 +0300

    f 1

--- WebLibraryApp/backend/LibraryApi/obj/Debug/net8.0/LibraryApp.AssemblyInfo.cs
+++ WebLibraryApp/backend/LibraryApi/obj/Debug/net8.0/LibraryApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc68b6355ac2fc33d2b72d5e45acc18ac3a63557")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
